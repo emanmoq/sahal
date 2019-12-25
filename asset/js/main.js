fb1e324299a9fcd7eae8dc17983645496a049da8
emanmoq <42995029+emanmoq@users.noreply.github.com>
2019-12-25 15:39:56 +0200

    ddd

---
+++ asset/js/main.js
@@ -0,0 +1,99 @@
+$(document).ready(function () {   
+   new WOW().init();
+
+    $('.countNum').each(function() {
+        var $this = $(this),
+            countTo = $this.attr('data-count');
+        
+        $({ countNum: $this.text()}).animate({
+          countNum: countTo
+        },
+      
+        {
+      
+          duration: 1000,
+          easing:'linear',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+          }
+      
+        });  
+});
+$('select').selectric({
+  disableOnMobile: false,
+  dir: "rtl"
+  
+});
+$('.clientSlider').owlCarousel({
+  loop:true,
+  margin:10,
+items:1,
+rtl:true
+
+
+});
+$('.PartnerSlider').owlCarousel({
+  loop:true,
+  margin:10,
+responsive:{
+0:{
+  items:2
+},
+600:{
+  items:3
+},
+1000:{
+  items:5
+}
+},
+rtl:true
+});
+$(".nav-item").click(function(){
+  $(".nav-item").removeClass("active");
+  $(this).addClass("active");
+});
+$(window).scroll(function () {
+  scrollTop = $(window).scrollTop();
+
+   if( scrollTop >$('header').height()){
+      $('header').addClass('scrollNav');
+
+
+   }
+   else{
+      $('header').removeClass('scrollNav');    
+
+   }
+   
+
+  });
+  function onScroll(event){
+			
+    var scrollPosition = $(document).scrollTop();
+    $('.nav-item a[href^="#"').each(function () {
+      console.log($(this).attr('href')); 
+      var refElement = $($(this).attr("href"));
+    //   console.log($(this).attr("href")); //log
+      if (refElement.length&&refElement.position().top <= scrollPosition) {
+      $('.nav-item ').removeClass("active");
+      $(this).parent().addClass("active");
+      }
+      else{
+      $(this).removeClass("active");
+      
+      }
+  
+    });
+    }
+    $(document).on("scroll", onScroll);
+    $('.navbar-collapse ul li a').on('click', function () {
+      $('html, body').animate({ scrollTop: $(this.hash).offset().top - 5 }, 1000);
+      $('.navbar-collapse.collapse.in').removeClass('in');
+      return false;
+    });
+});
+
+                
